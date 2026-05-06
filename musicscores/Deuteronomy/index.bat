e2531f47ea7437dab5a2256dee342de547944032
stenagmois-byte <stenagmois@gmail.com>
2026-05-05 22:40:59 -0700

    Get all asset links in place
    Write batch files to create index html files at each required level

---
+++ musicscores/Deuteronomy/index.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal enabledelayedexpansion
+
+echo ^<html^> > index.html
+echo ^<head^>^<meta charset="UTF-8"^>^<title^>Index^</title^>^</head^> >> index.html
+echo ^<body^> >> index.html
+
+echo ^<h1^>Deuteronomy^</h1^> >> index.html
+echo ^<ul^> >> index.html
+
+for %%f in (*.mscz) do (
+    set "name=%%~nf"
+    set "chap=!name:~-3!"
+    echo ^<li^>^<a href="%%f"^>Deuteronomy !chap!^</a^>^</li^> >> index.html
+)
+
+echo ^</ul^> >> index.html
+echo ^<p^>^<a href="../index.html"^>Back^</a^>^</p^> >> index.html
+
+echo ^</body^>^</html^> >> index.html
