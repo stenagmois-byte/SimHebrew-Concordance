a126b55b3d57709406ad8a511291be86a8e0ea10
stenagmois-byte <stenagmois@gmail.com>
2026-06-16 20:55:45 -0700

    convert to mxl and update bat and index files

--- musicscores/Isaiah/index.bat
+++ musicscores/Isaiah/index.bat
@@ -5,13 +5,26 @@ echo ^<html^> > index.html
 echo ^<head^>^<meta charset="UTF-8"^>^<title^>Index^</title^>^</head^> >> index.html
 echo ^<body^> >> index.html
 
-echo ^<h1^>Isaiah^</h1^> >> index.html
+for %%I in (.) do set "CurrentDir=%%~nxI"
+echo ^<h1^>%CurrentDir%^</h1^> >> index.html
+
+echo ^<h2^>MSCZ^</h2^> >> index.html
 echo ^<ul^> >> index.html
 
 for %%f in (*.mscz) do (
     set "name=%%~nf"
     set "chap=!name:~-3!"
-    echo ^<li^>^<a href="%%f"^>Isaiah !chap!^</a^>^</li^> >> index.html
+    echo ^<li^>^<a href="%%f" download^>Chapter !chap!^</a^>^</li^> >> index.html
+)
+
+echo ^</ul^> >> index.html
+echo ^<h2^>Compressed MusicXML (.mxl)^</h2^> >> index.html
+echo ^<ul^> >> index.html
+
+for %%f in (*.mxl) do (
+    set "name=%%~nf"
+    set "chap=!name:~-3!"
+    echo ^<li^>^<a href="%%f" download^>Chapter !chap!^</a^>^</li^> >> index.html
 )
 
 echo ^</ul^> >> index.html
